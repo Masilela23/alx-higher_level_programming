941ac043381c0202f2605df6587c398405bbfe5a
Masilela23 <melusi.mokone@gmail.com>
2024-01-04 12:04:29 -0800

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from hidden_4 import *
 
-    import hidden_4
-
-names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "_":
+            print("{:s}".format(allf[i]))
