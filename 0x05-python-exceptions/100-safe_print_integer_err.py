91ba2817017c08b03c8a8be42d8e2036e4adbfb3
Masilela23 <melusi.mokone@gmail.com>
2024-01-22 13:42:33 -0800

    Advanced

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-import sys
-
 
 def safe_print_integer_err(value):
-    
+    from sys import stderr
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+    except Exception as TE:
+        print('Exception: {}'.format(TE), file=stderr)
+        return False
+    return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-from __future__ import print_function
-
-import sys
 
 def safe_function(fct, *args):
+    from sys import stderr
     try:
-        res = fct(*args)
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
-    else:
-        return 
+        result = fct(*args)
+        return result
+    except Exception as err:
+        print('Exception: {}'.format(err), file=stderr)
+        return None 
